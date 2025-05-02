d8a06701600de2d055172b07b0e1124c8b4b7120
raggedpuppet915 <107402601+raggedpuppet915@users.noreply.github.com>
2025-05-02 16:05:19 -0500

    Remove sensitive files from Git tracking

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  oauthConsumerKey: '3MVG9Rr0EZ2YOVMY.q_lfmXoUtAz5_O1pSg9tc4JjbP0OpL6CS.1fezXv2QHY2hXUe6U.9U0q3eUHpCHWM8c9',
-  oauthRedirectURI: 'http://localhost:3000/oauthcallback.html',
-  loginUrl: 'https://login.salesforce.com'
-};
